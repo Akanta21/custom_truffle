69060df256a2c0928aa7b17d90ab356a094641c5
Andrian Kanta <a.kanta22@gmail.com>
2022-12-29 10:09:45 +0800

    updated the solc version

--- deploy.js
+++ deploy.js
@@ -1,1 +1,32 @@
 // deploy code will go here
+require('dotenv').config()
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+const {abi, evm} = require('./compile');
+
+const {MNEMONIC, NETWORK} = process.env;
+
+const provider = new HDWalletProvider(
+    MNEMONIC,
+    NETWORK
+)
+
+const web3 = new Web3(provider);
+
+// list account
+const deploy = async() => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account:', accounts[0])
+
+    const contractAddress = await new web3.eth.Contract(abi)
+        .deploy({data: evm.bytecode.object, arguments: ['This is initial message']})
+        .send({gas:'1000000', from: accounts[0]})
+
+    console.log('contractAddress', contractAddress.options.address)
+    provider.engine.stop()
+}
+
+
+// deploy
+deploy()
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,39 @@
 // contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const web3 = new Web3(ganache.provider());
+const { abi, evm } = require('../compile');
+
+let acc, inbox;
+const initial = 'Hi there'
+
+beforeEach(async() => {
+    // Get list of accounts
+    const accounts = await web3.eth.getAccounts()
+    // Use one to deploy contract
+    acc = accounts[0]
+
+    inbox = await new web3.eth.Contract(abi)
+        .deploy({data: evm.bytecode.object, arguments:[initial]})
+        .send({from: acc, gas: '1000000'})
+})
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address);
+    })
+
+    it('has a default message', async() => {
+        const initialMessage = await inbox.methods.message().call();
+        assert.equal(initialMessage, initial);
+    })
+
+    it('can change message', async() => {
+        const changeMessage = 'New Message'
+        await inbox.methods.setMessage(changeMessage).send({from: acc});
+        const message = await inbox.methods.message().call();
+        assert.equal(message, changeMessage);
+    })
+})
